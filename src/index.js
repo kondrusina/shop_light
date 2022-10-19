96a8dc7e80e2c9e70f6fa37492730e58f6d796d9
kondrusina <kondrusina@rambler.ru>
2022-10-19 19:24:05 +0300

    create section main__left, add content

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './index.html';
 import './index.scss';
+
+const button = document.querySelector('.btn');
+
+feach('https://private-anon-74061ac3bc-lampshop.apiary-mock.com/lamps');
+console.log(feach)
